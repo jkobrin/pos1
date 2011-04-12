9f07d98f7efd982b0a0ca24a339302073e24770a
jkobrin <jkobrin@pos.(none)>
2011-04-12 15:55:22 -0400

    add sold report

---
+++ sold.py
@@ -0,0 +1,38 @@
+
+import json
+import MySQLdb
+import utils
+
+
+
+def index(req):
+
+  results = utils.select('''
+    SELECT 
+	item_name, count(*) count, DATE(created) 
+	from order_item where is_cancelled = false 
+	group by item_name, DATE(created)
+	order by DATE(created), item_name;
+  ''',
+    incursor=None,
+    label=False
+  )
+
+  
+  html = (
+    '''  
+      <html>
+      <body>
+    ''' + 
+    utils.tohtml(
+      'items sold',
+      ('name', 'count', 'date'), 
+      results
+    ) +
+    '''</body></html>'''
+  )
+
+  return html
+
+if __name__ == '__main__':
+  print 'hi'
