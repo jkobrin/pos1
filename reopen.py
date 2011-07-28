d4598b7a54f8f7c066f3a416484c8cd28510a7b1
joshua kobrin <jkobrin@zareason-strata.(none)>
2011-07-28 19:09:18 -0400

    reopen fix

---
+++ reopen.py
@@ -0,0 +1,37 @@
+
+import json
+import MySQLdb
+import utils
+
+
+def get_reopen_id(req, table):
+
+  results = utils.select(
+    '''select id
+       from order_group og
+       where og.table_id = "%(table)s"
+       and og.is_open = False 
+       and og.updated > now() - INTERVAL '20' minute''' % locals()
+  )
+
+  if results:
+    retval = results[0]['id']
+  else:
+    retval = None
+
+  return json.dumps(retval)
+
+
+def index(req, reopen_id):
+
+  utils.execute(
+    '''update order_group
+       set is_open = True
+       where id = "%(reopen_id)s"''' % locals()
+  )
+
+  return json.dumps(None)
+
+
+if __name__ == '__main__':
+  print index(None)
