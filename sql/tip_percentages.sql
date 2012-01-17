061a365011db88f395705af91ee2557a0d3fbde2
jkobrin <jkobrin@pos.(none)>
2012-01-17 15:15:54 -0500

    fix timereport and minor mods

--- sql/tip_percentages.sql
+++ sql/tip_percentages.sql
@@ -1,4 +1,4 @@
-set @the_tip = 201;
+set @the_tip = 173;
 
 create or replace view last_night_items
 as
@@ -6,12 +6,16 @@ select * from order_item
 where is_cancelled = False
   and item_name not like 'gift%'
   and created > now() - INTERVAL '24' HOUR
-  #and date(created- interval '8' hour) =  '2011-12-26'
+  #and date(created- interval '8' hour) =  '2012-1-11'
 ;
 
 create or replace view person_items
 as
 select lni.id as lni_id, lni.price, p.id as p_id, last_name
+  , if(p.last_name = 'Barbagallo', 0,
+    if(p.last_name = 'Smith', .5, 
+    if(p.last_name = 'Addy', .5, 
+        1)))person_share
 from 
   last_night_items lni
   ,hours h
@@ -22,10 +26,10 @@ where h.person_id = p.id
 
 create or replace view item_split
 as
-select lni_id, price / (count(p_id)) split_price from person_items group by lni_id; 
+select lni_id, price / sum(person_share) split_price from person_items group by lni_id; 
 
 
-select last_name, sum(split_price)/(select sum(price) from last_night_items) * @the_tip
+select last_name, sum(split_price)/(select sum(price) from last_night_items) * @the_tip * person_share
 from 
   item_split spl
   ,person_items pi
