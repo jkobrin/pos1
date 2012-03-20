073272e805b002f26686bfda66e8137eb2d8908f
joshua kobrin <jkobrin@zareason-strata.(none)>
2012-03-20 14:50:23 -0400

    right button / mod time

--- action.py
+++ action.py
@@ -68,7 +68,7 @@ def order(req, table, additem=None, removeitem=None, price=None):
       oi.item_name, og.table_id, 
       oi.id, oi.is_delivered, oi.is_held, oi.is_comped, oi.price,
       TIMESTAMPDIFF(MINUTE, oi.created, now()) minutes_old,
-      TIMESTAMPDIFF(MINUTE, oi.updated, now()) minutes_closed
+      TIMESTAMPDIFF(MINUTE, oi.updated, now()) minutes_since_mod
     FROM order_group og, order_item oi 
     where og.id = oi.order_group_id
     and og.is_open = TRUE
