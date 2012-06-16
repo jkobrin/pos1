2c7482a7f1646bbaa727d3b1c18b677e9ef67413
jkobrin <jkobrin@pos.(none)>
2012-06-16 15:15:30 -0400

    textab changes for closed check report

--- sql/tip_percentages.sql
+++ sql/tip_percentages.sql
@@ -1,5 +1,5 @@
 #TODO: exclude lunch the following day
-set @the_tip = 632.94;
+set @the_tip = 620.48;
 
 create or replace view last_night_items
 as
@@ -15,12 +15,12 @@ as
 select lni.id as lni_id, lni.price, p.id as p_id, last_name
   , if(p.last_name = 'Kobrin', .7,
     if(p.last_name = 'Pampalone', 1, 
-    if(p.last_name = 'Ponce', .5, 
+    if(p.last_name = 'Ponce', 1, 
     if(p.last_name = 'Smith', .5,
     if(p.last_name = 'Labossier', 1,
     if(p.last_name = 'Salazar', 1, 
     if(p.last_name = 'Lilli', 1,
-    if(p.last_name = 'Young', .5,
+    if(p.last_name = 'Young', 1,
     if(p.last_name = 'DiLemme', 1, 
     if(p.last_name = 'Addy', 0, 
         1))))))))))person_share
@@ -36,7 +36,7 @@ create or replace view item_split
 as
 select lni_id, price / sum(person_share) split_price from person_items group by lni_id; 
 
-select '--------',dayname(min(created)), date(min(created)),'------------' from last_night_items;
+select '--------',dayname(min(created)), date(min(created)),'------------*(not yet paid out)' from last_night_items;
 select last_name, round(sum(split_price)/(select sum(price) from last_night_items) * @the_tip * person_share)
 from 
   item_split spl
