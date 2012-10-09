91481f235a3c2f190fce2cae509c026036ae1f03
jkobrin <jkobrin@pos.(none)>
2012-10-09 18:13:17 -0400

    various

--- sql/tip_percentages.sql
+++ sql/tip_percentages.sql
@@ -1,6 +1,6 @@
 #TODO: exclude lunch the following day....DONE!
 
-set @the_tip = 717.0 -413.68*.03;
+set @the_tip = 196.94 - (91.04)*.03;
 
 create or replace view last_night_items
 as
@@ -11,13 +11,19 @@ where is_cancelled = False
   and (time(created) > '16:00:00' or time(created) < '04:00:00')
 ;
 
+#create or replace view lni_order_and_deliver
+#as
+#(select id, price, created as tip_time from last_night_items)
+#union all
+#(select id, price, updated as tip_time from last_night_items)
+
 create or replace view person_items
 as
 select lni.id as lni_id, lni.price, p.id as p_id, last_name
-  , if(p.last_name = 'Kanarova', .6,
+  , if(p.last_name = 'Kobrin', 1,
     if(p.last_name = 'Addy', 0, 
     if(p.last_name = 'Ponce',.5, 
-    if(p.last_name = 'Cohen', 1,
+    if(p.last_name = 'Kanarova', .6,
     if(p.last_name = 'Salazar', 1,
     if(p.last_name = 'Young', .5, 
     if(p.last_name = 'Labossier', 1, 
@@ -29,8 +35,7 @@ from
   ,hours h
   ,person p
 where h.person_id = p.id
-  and lni.created between h.intime and h.outtime
-;
+  and lni.created between h.intime and h.outtime;
 
 create or replace view item_split
 as
