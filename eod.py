ec0b4b49db5dac63a8b131f8f1ea22863057abcc
jkobrin <jkobrin@pos.(none)>
2013-03-20 16:28:46 -0400

    various

--- eod.py
+++ eod.py
@@ -9,23 +9,23 @@ def index(req, lag=0):
 
   results = queries.nightly_sales_by_server(lag_days=lag)
 
-  seven_day_total = utils.select('''
-    SELECT sum(total) night, sum(dtotal) lunch  
-    FROM nd_tots  
-    WHERE dat > now() - INTERVAL '7' DAY''',
-    incursor=None,
-    label=False
-  )
+  #seven_day_total = utils.select('''
+  #  SELECT sum(total) night, sum(dtotal) lunch  
+  #  FROM nd_tots  
+  #  WHERE dat > now() - INTERVAL '7' DAY''',
+  #  incursor=None,
+  #  label=False
+  #)
 
-  avg = utils.select('''
-    SELECT sum(total)/2 night, sum(dtotal)/2 lunch  
-    FROM nd_tots  
-    WHERE dat > now() - INTERVAL '14' DAY;''',
-    incursor=None,
-    label=False
-  )
+  #avg = utils.select('''
+  #  SELECT sum(total)/2 night, sum(dtotal)/2 lunch  
+  #  FROM nd_tots  
+  #  WHERE dat > now() - INTERVAL '14' DAY;''',
+  #  incursor=None,
+  #  label=False
+  #)
 
-  day_totals = utils.select('''select * from nd_tots order by dat desc''', incursor=None, label=False)
+  #day_totals = utils.select('''select * from nd_tots order by dat desc''', incursor=None, label=False)
   
   grand_total = utils.select('''
     SELECT total, dtotal  
@@ -51,22 +51,22 @@ def index(req, lag=0):
       grand_total
     ) +
 
-    utils.tohtml(
-      "7 day Total",
-      ('Dinner','Lunch'), 
-      seven_day_total
-    ) +
-    utils.tohtml(
-      "2 week average",
-      ('Dinner','Lunch'), 
-      avg
-    ) +
+   # utils.tohtml(
+   #   "7 day Total",
+   #   ('Dinner','Lunch'), 
+   #   seven_day_total
+   # ) +
+   # utils.tohtml(
+   #   "2 week average",
+   #   ('Dinner','Lunch'), 
+   #   avg
+   # ) +
 
-    utils.tohtml(
-      "Day Totals",
-      ('Dinner', 'Day', 'Date','Lunch'), 
-      day_totals
-    ) +
+#    utils.tohtml(
+#      "Day Totals",
+#      ('Dinner', 'Day', 'Date','Lunch'), 
+#      day_totals
+#    ) +
     '''</body></html>'''
 
   )
