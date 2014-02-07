4733173ed838fc682ea94705c1df6106388dcd24
joshua kobrin <jkobrin@salsrv.(none)>
2014-02-07 10:06:52 -0500

    site specific gift certs

--- print_gift_cert.sh
+++ print_gift_cert.sh
@@ -2,4 +2,4 @@
 
 sed -e "s/REPLACE_SERIAL/$1/g; s/REPLACE_DENOM/$2/g; s/REPLACE_RJUST_DENOM/$3/g" $(hostname)_moneymaster.ps > /var/www/certs/$4.ps
 lp /var/www/certs/$4.ps
-gs -sDEVICE=pngmono -sOutputFile=/var/www/certs/$4.png /var/www/certs/$4.ps
+#gs -sDEVICE=pngmono -sOutputFile=/var/www/certs/$4.png /var/www/certs/$4.ps
