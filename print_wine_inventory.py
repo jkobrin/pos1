f45093ed36abde65b14eb8654f4267d78fe12a4b
joshua kobrin <jkobrin@salumibarli.com>
2016-12-24 15:17:40 -0500

    cellarlist print function

--- print_wine_inventory.py
+++ print_wine_inventory.py
@@ -33,5 +33,30 @@ def index(req = None):
     os.remove(filename)
     return  json.dumps(None)
 		
+def cellar_list(req = None):
+    cellarlist = utils.select('''
+      select bin as binnum, name, round(estimated_units_remaining) est_count, byline, listprice
+      from winelist_inv where bin !=0 and cellar_listorder != 0 and cellar_listorder is not null
+      order by cellar_listorder;
+    '''
+    , label=True)
+
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    filename = outfile.name
+    for rec in cellarlist:
+      outfile.write(
+        rec['name'].encode('utf8') + ' ' + 
+        (rec['byline'] or '').encode('utf8') + '\n' +
+        'bin '+str(rec['binnum']) + ', ' +
+        str(int(rec['est_count'])) + ' bottles' + ',  $' + str(rec['listprice']) + '\n\n'
+      )
+
+    outfile.close()
+    #subprocess.call(['enscript', '--font=Courier-Bold@11/16', '-B', '-MEnv10', filename])
+    subprocess.call(['cat', filename])
+    os.remove(filename)
+    return  json.dumps(None)
+		
 if __name__ == '__main__':
-    index()
+    #index()
+    cellar_list()
