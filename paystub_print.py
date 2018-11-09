0e1bc4900295005bbf9f041a68d75aeea178e9e0
Joshua Kobrin <joshua@plansrv.(none)>
2018-11-09 12:40:38 -0500

    waiting descriptors and coupon stuff

--- paystub_print.py
+++ paystub_print.py
@@ -193,4 +193,4 @@ def make_estub(first_name, last_name, baserate, rate_variance, basehours, hour_v
     print_stubs(0, sunday, table_name, incursor=incursor)
 
 if __name__ == '__main__':
-  print_recent_weekly('Seney')
+  print_recent_weekly('Pascal')
